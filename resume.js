feba19237df1061dcf8503efd6c9372bd42e7e08
Fidelis <fideliofidel9@gmail.com>
2023-02-10 21:25:42 +0300

    pdf embed added

--- resume.js
+++ resume.js
@@ -8,3 +8,9 @@ const resumeTitle = document.createElement('h2');
 resumeTitle.textContent = `Resume`;
 resumeDiv.appendChild(resumeTitle);
 
+const pdfView = document.createElement('embed');
+pdfView.setAttribute('src', 'FRANKLIN_CV.pdf');
+pdfView.setAttribute('width', '100%')
+pdfView.setAttribute('height', '600px')
+pdfView.className = 'pdf-view';
+resumeDiv.appendChild(pdfView);
