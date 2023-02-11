93670dc7fcaf20f4466c717526cced6c05afcbc8
Fidelis <fideliofidel9@gmail.com>
2023-02-11 08:10:26 +0300

    pdf view changed

--- resume.js
+++ resume.js
@@ -8,9 +8,19 @@ const resumeTitle = document.createElement('h2');
 resumeTitle.textContent = `Resume`;
 resumeDiv.appendChild(resumeTitle);
 
-const pdfView = document.createElement('embed');
+// image
+const img1 = document.createElement('img');
+img1.setAttribute('src', './frank_cv/Frank-cv1.svg');
+resumeDiv.appendChild(img1);
+
+// image 2
+const img2 = document.createElement('img');
+img2.setAttribute('src', 'frank_cv/Frank-cv2.svg');
+resumeDiv.appendChild(img2);
+
+/* const pdfView = document.createElement('embed');
 pdfView.setAttribute('src', 'FRANKLIN_CV.pdf');
 pdfView.setAttribute('width', '100%');
 pdfView.setAttribute('height', '600px');
 pdfView.className = 'pdf-view';
-resumeDiv.appendChild(pdfView);
+resumeDiv.appendChild(pdfView); */
